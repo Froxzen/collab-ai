6fbb6154c9de5bc3ddf0bc72cfc9a0a65a84a746
Froxzen <harvardchong1@gmail.com>
2024-12-18 09:25:00 +1100

    Added UI button for Sign In

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import { SignInButton, SignedOut, SignedIn, UserButton } from "@clerk/nextjs";
+import { Logo } from "./logo";
+import { Button } from "./ui/button";
 
 const Navbar = () => {
-  return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+	return (
+		<div className="my-2 mx-4 flex justify-between items-center py-2">
+      <Logo />
+			<div>
+      <SignedOut>
+        <SignInButton>
+          <Button>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      </div>
+		</div>
+	);
+};
 
-export default Navbar
+export default Navbar;
+
+// 26:53
