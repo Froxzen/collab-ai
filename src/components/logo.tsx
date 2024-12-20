9fec3804362085b820f663265208847f586e52d9
Froxzen <harvardchong1@gmail.com>
2024-12-21 01:37:19 +1100

    Changed name

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -11,7 +11,7 @@ export const Logo = () => {
       <Link href={"/"}>
         <Image src="/logo.svg" height={40} width={40} alt="logo" />
       </Link>
-      <h2 className={cn("text-2xl ml-2", karla.className)}>Quill Wizards</h2>
+      <h2 className={cn("text-2xl ml-2", karla.className)}>Collab AI</h2>
     </div>
   );
 };
